88aa52a265c89f46af091a2f9219ba4de59982df
zouhuayu1 <zouhuayu1@jd.com>
2021-06-09 13:56:52 +0800

    branch004提交 001

---
+++ gittest/src/main/java/com/zouhuayu/test1/Branch003Test.java
@@ -0,0 +1,12 @@
+package com.zouhuayu.test1;
+
+/**
+ * @Author: zouhuayu1
+ * @Date: 2021/6/9 下午1:54
+ */
+public class Branch003Test {
+    // 在master分支新建分支branch004,开发分支Branch004Test
+    public static void main(String[] args) {
+        System.out.println("branch004 111111111");
+    }
+}
