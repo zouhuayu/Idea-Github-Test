98bdb7a93c9ba3b22b0f91d840cf70710cac9241
zouhuayu1 <zouhuayu1@jd.com>
2021-06-09 14:02:51 +0800

    branch003提交 002

--- gittest/src/main/java/com/zouhuayu/test1/Branch004Test.java
+++ gittest/src/main/java/com/zouhuayu/test1/Branch004Test.java
@@ -4,9 +4,11 @@ package com.zouhuayu.test1;
  * @Author: zouhuayu1
  * @Date: 2021/6/9 下午1:54
  */
-public class Branch003Test {
+public class Branch004Test {
     // 在master分支新建分支branch004,开发分支Branch004Test
     public static void main(String[] args) {
         System.out.println("branch004 111111111");
+        // 我在分支004上面新建一个分支005，然后对代码进行更新，之后合并到分支003
+        System.out.println("branch005 dev then merge into branch003");
     }
 }
