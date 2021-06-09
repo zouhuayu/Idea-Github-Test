8133866860e4d6ef1f570936ff0b1b11db92e0ad
zouhuayu1 <zouhuayu1@jd.com>
2021-06-09 14:18:02 +0800

    branch006提交 002

--- gittest/src/main/java/com/zouhuayu/test1/Branch006.java
+++ gittest/src/main/java/com/zouhuayu/test1/Branch006.java
@@ -7,5 +7,7 @@ package com.zouhuayu.test1;
  */
 public class Branch006 {
     //我在本地新建一个分支006，拉取005代码，目的是直接在005上面开发，自己不生成远程006分支
-
+    public static void main(String[] args) {
+        System.out.println("怎么默认push到branch005?");
+    }
 }
