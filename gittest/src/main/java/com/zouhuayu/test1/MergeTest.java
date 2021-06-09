58483b20c1d2affc87261878d5715bbdb1346aca
zouhuayu1 <zouhuayu1@jd.com>
2021-06-09 13:46:32 +0800

    branch002提交 003

--- gittest/src/main/java/com/zouhuayu/test1/MergeTest.java
+++ gittest/src/main/java/com/zouhuayu/test1/MergeTest.java
@@ -10,6 +10,8 @@ public class MergeTest {
         // 试验一下利用 branch1合并branch001
         // 新建一个branch002分支，写新代码，提交到远程branch002分支
         System.out.println("hello branch002  1111111");
-        System.out.println("hello branch002  2222222");
+        System.out.println("hello branch002  2222222"); // 利用版本号可以实现分支代码回退
+        System.out.println("看到这行代码，说明我把分支branch002的开发代码成功合并到了主分支");
+
     }
 }
