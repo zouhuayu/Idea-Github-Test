a3e432e33d4cf1683222fde9084d7526f1c85fc0
zouhuayu1 <zouhuayu1@jd.com>
2021-06-09 13:22:20 +0800

    branch提交 002

--- gittest/src/main/java/com/zouhuayu/test1/HelloWorld.java
+++ gittest/src/main/java/com/zouhuayu/test1/HelloWorld.java
@@ -5,4 +5,8 @@ package com.zouhuayu.test1;
  * @Date: 2021/6/9 上午11:40
  */
 public class HelloWorld {
+    // 先在远程仓库新建一个分支，本地也新建一份，修改代码代码，提交到远程新分支。
+    public static void main(String[] args) {
+
+    }
 }
